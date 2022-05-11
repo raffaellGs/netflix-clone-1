97bfb17378ddfed05338bc6c61746cc1c56b860d
Rafael <rafael111515@gmail.com>
2022-05-11 16:39:01 -0300

    Ajeitando o Responsivo e deixando mais atraente

--- assets/js/owl/setup.js
+++ assets/js/owl/setup.js
@@ -2,15 +2,16 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 5,
     nav: false,
+    dots:false,
     responsive: {
         0: {
-            items: 1
-        },
-        600: {
             items: 3
         },
-        1000: {
+        600: {
             items: 5
+        },
+        1000: {
+            items: 10
         }
     }
 })
